dcfefa00ff5eee42ea6dd0b2bc3abf8624f161db
shiiku419 <shiiku_419@softbank.ne.jp>
2023-02-06 17:57:31 +0900

    graph noise

--- src/App.vue
+++ src/App.vue
@@ -4,69 +4,34 @@
       <v-toolbar>
         <v-toolbar-title>
           <span>HeartRate</span>
-          <span
-            v-if="!$vuetify.breakpoint.smAndDown"
-            class="font-weight-light ml-1"
-            >Graph</span
-          >
+          <span v-if="!$vuetify.breakpoint.smAndDown" class="font-weight-light ml-1">Graph</span>
         </v-toolbar-title>
-        <v-btn
-          class="ml-4"
-          rounded
-          text
-          :disabled="!isBluetoothAvailable()"
-          @click="connect"
-        >
+        <v-btn class="ml-4" rounded text :disabled="!isBluetoothAvailable()" @click="connect">
           Connect
         </v-btn>
         <v-spacer></v-spacer>
         <v-btn text rounded v-if="!!installPrompt" @click="installPWA">
           <v-icon>mdi-download</v-icon>Install
         </v-btn>
-        <v-btn
-          v-if="!$vuetify.breakpoint.smAndDown"
-          rounded
-          text
-          href="https://github.com/henkelmax/heartrate-graph"
-          target="_blank"
-        >
+        <v-btn v-if="!$vuetify.breakpoint.smAndDown" rounded text href="https://github.com/henkelmax/heartrate-graph"
+          target="_blank">
           <v-icon class="mr-2">mdi-open-in-new</v-icon>GitHub
         </v-btn>
       </v-toolbar>
 
       <v-row v-if="chartData.length <= 1" justify="center" class="ma-2">
         <v-col xl="6" lg="6" md="6" sm="12" cols="12">
-          <v-alert
-            v-if="isBluetoothAvailable()"
-            border="top"
-            colored-border
-            type="info"
-            elevation="2"
-            cols="6"
-          >
+          <v-alert v-if="isBluetoothAvailable()" border="top" colored-border type="info" elevation="2" cols="6">
             You need to connect to a heart rate monitor in order to receive
             data. Press the 'Connect' button to scan for Bluetooth devices.
           </v-alert>
-          <v-alert
-            v-else
-            border="top"
-            colored-border
-            type="error"
-            elevation="2"
-          >
+          <v-alert v-else border="top" colored-border type="error" elevation="2">
             Your browser is not able to connect with Bluetooth devices!
           </v-alert>
         </v-col>
       </v-row>
 
-      <GChart
-        v-else
-        class="chart mt-4"
-        type="LineChart"
-        :data="chartData"
-        :options="chartOptions"
-        fill-height
-      />
+      <GChart v-else class="chart mt-4" type="LineChart" :data="chartData" :options="chartOptions" fill-height />
 
       <v-snackbar v-model="snackbar">
         {{ text }}
@@ -83,6 +48,21 @@
 <script>
 import moment from "moment";
 
+let noise = 0;
+let i = 1;
+let past = {}
+
+document.addEventListener('keypress', keypress_ivent);
+
+function keypress_ivent(e) {
+  //いずれかのキーが押された時の処理
+  if (e.code === 'Space') {
+    noise += 1
+  } else {
+    noise -= 1
+  }
+}
+
 export default {
   name: "App",
   data: () => ({
@@ -92,7 +72,7 @@ export default {
     chartData: [["Time", "Heart Rate"]],
     chartOptions: {
       curveType: "function",
-      vAxis: { viewWindow: { min: 0 } },
+      vAxis: { viewWindow: { min: 40 } },
     },
     characteristic: null,
     installPrompt: null,
@@ -146,11 +126,12 @@ export default {
       let index = 1;
       if (rate16Bits) {
         result.heartRate = value.getUint16(index, true);
-        index += 2;
+        index += 2.0;
       } else {
         result.heartRate = value.getUint8(index);
-        index += 1;
+        index += 1.0;
       }
+      result.heartRate += noise
       let contactDetected = flags & 0x2;
       let contactSensorPresent = flags & 0x4;
       if (contactSensorPresent) {
@@ -159,7 +140,7 @@ export default {
       let energyPresent = flags & 0x8;
       if (energyPresent) {
         result.energyExpended = value.getUint16(index, true);
-        index += 2;
+        index += 2.0;
       }
       let rrIntervalPresent = flags & 0x10;
       if (rrIntervalPresent) {
@@ -169,6 +150,7 @@ export default {
         }
         result.rrIntervals = rrIntervals;
       }
+      i += 1;
       return result;
     },
     onValueChanged(event) {
